aa3e940ef9845ed906e8a39e882ef2bb106a345d
David Slochower <slochower@gmail.com>
2017-07-14 13:00:53 -0700

    Fix paths in deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -21,14 +21,11 @@ git checkout $TRAVIS_BRANCH
 git remote set-url origin git@github.com:$TRAVIS_REPO_SLUG.git
 
 # Decrypt and add SSH key
-echo $PWD
-ls *
 openssl aes-256-cbc \
   -K $encrypted_49004ed0a784_key \
   -iv $encrypted_49004ed0a784_iv \
   -in ci/deploy.key.enc \
-  -out deploy.key -d
-ls *
+  -out ci/deploy.key -d
 eval `ssh-agent -s`
 chmod 600 ci/deploy.key
 ssh-add ci/deploy.key
